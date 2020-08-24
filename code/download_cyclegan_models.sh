a0fada9b3619eddc9d2b85db5944982b092b8b88
Ian <ianpradh@buffalo.edu>
2020-08-23 23:01:54 -0400

    download cyclegan models

--- code/download_cyclegan_models.sh
+++ code/download_cyclegan_models.sh
@@ -3,12 +3,12 @@
 echo "Note: available models are apple2orange, orange2apple, summer2winter_yosemite, winter2summer_yosemite, horse2zebra, zebra2horse, monet2photo, style_monet, style_cezanne, style_ukiyoe, style_vangogh, sat2map, map2sat, cityscapes_photo2label, cityscapes_label2photo, facades_photo2label, facades_label2photo, iphone2dslr_flower"
 
 
-declare -a arr=("apple2orange", "orange2apple", "summer2winter_yosemite",
- "winter2summer_yosemite",
- "horse2zebra", "zebra2horse", "monet2photo", "style_monet", "style_cezanne",
- "style_ukiyoe", "style_vangogh", "sat2map", "map2sat", "cityscapes_photo2label",
- "cityscapes_label2photo",
- "facades_photo2label", "facades_label2photo", "iphone2dslr_flower")
+declare -a arr=("apple2orange" "orange2apple" "summer2winter_yosemite"
+ "winter2summer_yosemite"
+ "horse2zebra" "zebra2horse" "monet2photo" "style_monet" "style_cezanne"
+ "style_ukiyoe" "style_vangogh" "sat2map" "map2sat" "cityscapes_photo2label"
+ "cityscapes_label2photo"
+ "facades_photo2label" "facades_label2photo" "iphone2dslr_flower")
 
 
 for FILE in "${arr[@]}"
